4ae9cdfc115918416391ba1a9cc7da4680b234d3
Kevin Christensen <nivekmai@gmail.com>
2023-05-27 10:52:45 -0700

    Adding buttons

--- PMW3360-Seeeduino-Trackball-Mouse.ino
+++ PMW3360-Seeeduino-Trackball-Mouse.ino
@@ -1,16 +1,38 @@
-#include <PMW3360.h>
 #include <Mouse.h>
+#include <PMW3360.h>
+#include <ezButton.h>
 
-PMW3360 sensor(5, 1600);
+#define DEBOUNCE 50
+
+ezButton buttonL(4);
+ezButton buttonM(2);
+ezButton buttonR(3);
+PMW3360 sensor(6, 1600);
 
 void setup() {
   Serial.begin(96000);
   Mouse.begin();
   sensor.setup();
+  buttonL.setDebounceTime(DEBOUNCE);
+  buttonM.setDebounceTime(DEBOUNCE);
+  buttonR.setDebounceTime(DEBOUNCE);
 }
 
 void loop() {
-  sensor.loop([](int x, int y){
-    Mouse.move(x,y,0);
+  buttonL.loop();
+  buttonM.loop();
+  buttonR.loop();
+  buttonLoop(buttonR, MOUSE_RIGHT);
+  buttonLoop(buttonM, MOUSE_MIDDLE);
+  buttonLoop(buttonL, MOUSE_LEFT);
   });
+}
+
+void buttonLoop(ezButton button, char mouseButton) {
+  if (button.isPressed()) {
+    Mouse.press(mouseButton);
+  }
+  if (button.isReleased()) {
+    Mouse.release(mouseButton);
+  }
 }
